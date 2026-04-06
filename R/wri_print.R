ae1c502a0054a5b5baaa8f0cb42877b31317f15c
kaimorquecho <kaimorquecho@ucsb.edu>
2026-04-06 13:53:15 -0700

    rewrite wri_overview() print message

--- R/wri_print.R
+++ R/wri_print.R
@@ -9,7 +9,7 @@ print.wri_overview <- function(x, ...) {
   cat("-------------------\n")
 
   if (!is.null(x$built_at)) {
-    cat("Read at:      ", format(x$built_at), "\n", sep = "")
+    cat("Read at:      ", format(x$built_at), "\n", sep = "", "\n")
   }
 
   cols <- x$data$collections
@@ -18,9 +18,11 @@ print.wri_overview <- function(x, ...) {
     return(invisible(x))
   }
 
+  cat("Layer ID Naming Convention: wri_domain + wri_dimension + data_type", "\n")
+
   collection_ids <- names(cols)
   cat("\nCollections (", length(collection_ids), "): ",
-      paste(collection_ids, collapse = ", "), "\n", sep = "")
+      paste(collection_ids, collapse = ", "), "\n", sep = "", "\n")
 
   col_obj <- cols[[1]]$collection
   sums <- col_obj$summaries
@@ -34,14 +36,14 @@ print.wri_overview <- function(x, ...) {
   domain_vals    <- sort(unique(unname(unlist(sums$wri_domain %||% character()))))
   dim_vals       <- sort(unique(unname(unlist(sums$wri_dimension %||% character()))))
 
-  cat("Unique data_type values (", length(data_type_vals), "): ",
-      paste(data_type_vals, collapse = ", "), "\n", sep = "")
   cat("Unique wri_domain values (", length(domain_vals), "): ",
-      paste(domain_vals, collapse = ", "), "\n", sep = "")
+      paste(domain_vals, collapse = ", "), "\n", sep = "", "\n")
+  cat("Unique data_type values (", length(data_type_vals), "): ",
+      paste(data_type_vals, collapse = ", "), "\n", sep = "", "\n")
   cat("Unique wri_dimension values (", length(dim_vals), "): ",
-      paste(dim_vals, collapse = ", "), "\n", sep = "")
+      paste(dim_vals, collapse = ", "), "\n", sep = "", "\n")
 
   item_ids <- names(x$data$items)
-  cat("Items: ", length(item_ids), "\n", sep = "")
+  cat("Total layers available: ", length(item_ids), "\n", sep = "")
 
 }
